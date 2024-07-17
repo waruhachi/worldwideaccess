aa86214a6324dbf1e5da58774a92e2f963ed3da7
waruhachi <156133757+waruhachi@users.noreply.github.com>
2024-07-16 21:20:14 -0500

    [feat] Removed NZ number

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,15 @@ export default function Footer() {
 	return (
 		<footer className="flex min-h-[35rem] w-full flex-col items-center justify-evenly bg-[#edc677] p-16 text-center text-black">
 			<Image src="/image/logo.png" alt="Logo" width={200} height={200} />
-			{/* <h1 className="mb-4 text-4xl">World Wide Access</h1> */}
 			<h1 className="text-2xl">
 				contact@worldwideaccess.org
 				<br />
 				<br />
-				US: +1 (469) 584-6588
+				AU: +61 481 877 282
 				<br />
-				NZ: +64 22 621 7282
+				US: +1 (469) 584-6588
 			</h1>
-			<div className="flex flex-row">
+			<div className="flex flex-row pt-4">
 				<Link
 					href="https://www.facebook.com"
 					className="mx-2"
